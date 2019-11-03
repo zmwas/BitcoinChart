2a34296897c2eb22dfe14b41ddf65682ffb04af4
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-11-03 11:46:29 +0300

    Add asserts

--- app/src/test/java/com/zack/bitcoinchart/viewmodel/BitcoinChartViewModelTest.kt
+++ app/src/test/java/com/zack/bitcoinchart/viewmodel/BitcoinChartViewModelTest.kt
@@ -4,6 +4,9 @@ import com.zack.data.model.ChartData
 import com.zack.data.model.Value
 import com.zack.domain.usecases.FetchBitcoinChartDataUsecase
 import io.reactivex.Single
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +14,7 @@ import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.CountDownLatch
 
 @RunWith(MockitoJUnitRunner::class)
 class BitcoinChartViewModelTest {
@@ -21,7 +25,12 @@ class BitcoinChartViewModelTest {
     @Before
     fun setUp() {
         viewModel = BitcoinChartViewModel(fetchBitcoinChartDataUsecase)
+        val value1 = Value(10f,123243546f)
+        val value2 = Value(10f,123243546f)
+
         val values = ArrayList<Value>()
+        values.add(value1)
+        values.add(value2)
         chartData = ChartData("ok","Market Price (USD)","USD","day",
             "Average USD market price across major bitcoin exchanges.", values)
 
@@ -29,10 +38,27 @@ class BitcoinChartViewModelTest {
 
     @Test
     fun testFetchingOfChartData() {
-
+        val latch = CountDownLatch(1)
+        var tempChartData: ChartData? = null
         Mockito.`when`(fetchBitcoinChartDataUsecase.execute())
             .thenReturn(Single.just(chartData))
-        viewModel.fetchChartData("1year","8hours")
+        viewModel.fetchChartData("1year","8hours").subscribeOn(Schedulers.io()).subscribe(
+            Consumer {
+                tempChartData = it
+                latch.countDown()
+            }
+        )
+        latch.await()
         verify(fetchBitcoinChartDataUsecase).execute()
+        Assert.assertEquals("ok", tempChartData!!.status)
+        Assert.assertEquals("Market Price (USD)", tempChartData!!.name)
+        Assert.assertEquals("USD", tempChartData!!.unit)
+        Assert.assertEquals("day", tempChartData!!.period)
+        Assert.assertEquals("Average USD market price across major bitcoin exchanges.",
+            tempChartData!!.description)
+        Assert.assertEquals(2,
+            tempChartData!!.values.size)
+
+
     }
 }
--- data/src/test/java/com/zack/data/repository/BitcoinChartRepositoryImplTest.kt
+++ data/src/test/java/com/zack/data/repository/BitcoinChartRepositoryImplTest.kt
@@ -4,6 +4,9 @@ import com.zack.data.api.BitcoinChartApiService
 import com.zack.data.model.ChartData
 import com.zack.data.model.Value
 import io.reactivex.Single
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,6 +15,7 @@ import org.mockito.Mockito
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.CountDownLatch
 
 @RunWith(MockitoJUnitRunner::class)
 class BitcoinChartRepositoryImplTest {
@@ -23,7 +27,13 @@ class BitcoinChartRepositoryImplTest {
     @Before
     fun setUp() {
         bitcoinChartRepositoryImpl = BitcoinChartRepositoryImpl(apiService)
+        val value1 = Value(10f,123243546f)
+        val value2 = Value(10f,123243546f)
+
         val values = ArrayList<Value>()
+        values.add(value1)
+        values.add(value2)
+
         chartData = ChartData(
             "ok", "Market Price (USD)", "USD", "day",
             "Average USD market price across major bitcoin exchanges.", values
@@ -33,9 +43,28 @@ class BitcoinChartRepositoryImplTest {
 
     @Test
     fun testFetchingOfDataFromApi() {
+        var tempChartData: ChartData? = null
+        val latch = CountDownLatch(1)
         Mockito.`when`(apiService.fetchBitCoinChartData(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
             .thenReturn(Single.just(chartData))
-        bitcoinChartRepositoryImpl.fetchChartData("1year", "8hours")
+        bitcoinChartRepositoryImpl.fetchChartData("1year", "8hours").subscribeOn(Schedulers.io()).subscribe(
+            Consumer {
+                tempChartData = it
+                latch.countDown()
+            }
+        )
+        latch.await()
         verify(apiService).fetchBitCoinChartData(ArgumentMatchers.anyString(), ArgumentMatchers.anyString())
+        Assert.assertEquals("ok", tempChartData!!.status)
+        Assert.assertEquals("Market Price (USD)", tempChartData!!.name)
+        Assert.assertEquals("USD", tempChartData!!.unit)
+        Assert.assertEquals("day", tempChartData!!.period)
+        Assert.assertEquals("Average USD market price across major bitcoin exchanges.",
+            tempChartData!!.description)
+        Assert.assertEquals(2,
+            tempChartData!!.values.size)
+
+
+
     }
 }
